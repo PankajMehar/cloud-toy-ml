d02eacd6d4f3f0f42b752b0e33b1b82f61ba7a1b
andydevs <akanshul97@gmail.com>
2018-06-21 23:30:27 -0400

    Initialized project

---
+++ setup.py
@@ -0,0 +1,17 @@
+"""
+Cloud Toy ML
+
+Toy training app to demonstrate machine learning in Google Cloud
+
+Author:  Anshul Kharbanda
+Created: 6 - 21 - 2018
+"""
+from distutils.core import setup
+
+# Setup function
+setup(name='cloud-toy-ml',
+      version='0.1',
+      description='Toy training app to demonstrate machine learning in gcloud',
+      author='Anshul Kharbanda',
+      author_email='akanshul97@gmail.com',
+      packages=['training'])
---
+++ training/__init__.py
@@ -0,0 +1,8 @@
+"""
+Cloud Toy ML
+
+Toy training app to demonstrate machine learning in Google Cloud
+
+Author:  Anshul Kharbanda
+Created: 6 - 21 - 2018
+"""
